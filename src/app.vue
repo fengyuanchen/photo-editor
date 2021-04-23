87e38a8c72441e4268ec8c5bbbf395b2fcbf0d8b
Dan Miron <dan-alexandru.miron@intel.com>
2021-04-23 10:51:56 +0300

    Add the ability to paste an image from the clipboard by issuing CTRL+V

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="app">
+  <div
+    class="app"
+    @paste="paste"
+  >
     <header class="header">
       <span class="title">Photo Editor</span>
       <navbar
@@ -39,6 +42,45 @@ export default {
   },
 
   methods: {
+    send_url_to_editor(url) {
+      let type = 'image/png';
+      if (url.includes('jpeg') || url.includes('jpg')) type = 'image/jpeg';
+      else if (url.includes('webp')) type = 'image/webp';
+      else if (url.includes('avif')) type = 'image/avif';
+      else if (url.includes('bmp')) type = 'image/bmp';
+
+      const pastedImgData = {
+        loaded: true,
+        name: 'pasted_image',
+        type,
+        url,
+      };
+      this.$refs.loader.update(pastedImgData);
+    },
+    paste(event) {
+      const { items } = event.clipboardData || event.originalEvent.clipboardData;
+      const app = this;
+      console.log('PASTE event fired.');
+      for (const index in items) {
+        const item = items[index];
+        if (item.kind === 'file') {
+          const blob = item.getAsFile();
+          const reader = new FileReader();
+          reader.onload = function (event) {
+            app.send_url_to_editor(event.target.result);
+            console.log(event.target.result);
+          };
+          reader.readAsDataURL(blob);
+        } else if (item.kind === 'string') {
+          item.getAsString((s) => {
+            if (s.startsWith('data:image') || s.startsWith('file:/')
+                            || s.endsWith('jpg') || s.endsWith('jpeg')
+                            || s.endsWith('png') || s.endsWith('bmp')
+                            || s.endsWith('webp') || s.endsWith('avif')) app.send_url_to_editor(s);
+          });
+        }
+      }
+    },
     change(action) {
       const { editor } = this.$refs;
 
